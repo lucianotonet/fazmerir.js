396dc9c3eeb20186bf5493eb3664dc8d41b1fa18
Luciano <tonetlds@gmail.com>
2015-06-14 16:50:49 -0300

    Tests with CDN

--- js/scripts.js
+++ js/scripts.js
@@ -185,3 +185,94 @@ $(function() {
     $('#fzmr_amount').val( r );
 
 });
+
+$( function(){
+
+    // Just for demo purposes...
+    //   See the documentation
+    
+    var cdn = 'https://cdn.rawgit.com/tonetlds/fazmerir.js/master/styles/';
+
+    $( '#demo' ).fazMeRir({                
+        'style' : 'default',
+        'stylespath': cdn,
+        'prefix': 'R$ ',
+        'centsSeparator': ',',
+        'thousandsSeparator': '.'
+    });
+
+    $('input').on('keyup', function(event) {                                    
+        var text = $('#fzmr_amount').val();
+        $('#demo').text( text ).fazMeRir({              
+            'style' : $( '#inputFazmerir_style option:selected' ).val(),
+            'stylespath': cdn,
+            'prefix': $('#fzmr_prefix').val(),
+            'suffix': $('#fzmr_suffix').val()
+        });     
+
+        $('#example pre').text( displaySnippet() );         
+
+    });
+
+    $('.price_format').priceFormat({
+        'prefix': '',
+        'centsSeparator': ',',
+        'thousandsSeparator': '.',
+        'suffix': '',
+    });
+
+
+
+    var styles = $( '#demo' ).fazMeRir.styles;
+    var select = $( '#inputFazmerir_style' );
+
+    styles.forEach(function(value, index){
+        select.append($("<option />")
+            .val( value )
+            .text( value ));
+    });
+
+    select.change(function(){
+        var style = $(this).val();
+        $("link#fazmerir_css").attr("href", cdn+'fazmerir.'+style+'.css' );
+
+        var text = $('#fzmr_amount').val() + "" + $('#fzmr_suffix').val();
+        $('#demo').text( text ).fazMeRir({              
+            'style' : $( '#inputFazmerir_style option:selected' ).val(),
+            'stylespath': cdn,
+            'prefix': $('#fzmr_prefix').val(),
+            'suffix': $('#fzmr_suffix').val()
+        });     
+
+        $('#example pre').text( displaySnippet() );
+        
+    }).change();
+
+
+    $.each( styles, function(index, val) {
+        console.log( val );
+    });
+
+
+    function displaySnippet(){
+        var output = "jQuery( 'your-element' ).fazMeRir({\n";                       
+
+            if( $( '#inputFazmerir_style option:selected' ).val().length ){
+                if( $( '#inputFazmerir_style option:selected' ).val() != 'default' ){
+                    output += " 'style' : '"+$( '#inputFazmerir_style option:selected' ).val()+"',\n";
+                }
+            }
+
+            if( $('#fzmr_prefix').val().length ){
+                output += " 'prefix' : '"+$('#fzmr_prefix').val()+"',\n";
+            }
+
+            if( $('#fzmr_suffix').val().length ){
+                output += " 'suffix' : '"+$('#fzmr_suffix').val()+"',\n";
+            }
+
+        output += "});\n";
+        return output;
+    }
+    
+}); 
